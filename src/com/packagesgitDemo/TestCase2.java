7b7737fa1bdec79506f827d2cb4cc5ed068184ce
mayur <mayur@LAPTOP-VIJQ9PPK>
2024-01-23 11:34:58 +0530

    Commiting Test 2 MayurBranchQA

---
+++ src/com/packagesgitDemo/TestCase2.java
@@ -0,0 +1,12 @@
+package com.packagesgitDemo;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Prem My Code is Done Please review the same");
+
+	}
+
+}
