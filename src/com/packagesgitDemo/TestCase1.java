69b38729746a3b1388df2046b60fff0ca314901d
premnath <premnath9714@gmail.com>
2024-01-23 10:20:30 +0530

    First Commit...

---
+++ src/com/packagesgitDemo/TestCase1.java
@@ -0,0 +1,12 @@
+package com.packagesgitDemo;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Mayur Start your Project");
+
+	}
+
+}
