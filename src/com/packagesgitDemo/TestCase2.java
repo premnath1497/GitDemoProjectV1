7dccaf2586723c7f5b9eb28c11fc63a450e80179
mayur <mayur@LAPTOP-VIJQ9PPK>
2024-01-23 12:10:35 +0530

    Additional Checks..

--- src/com/packagesgitDemo/TestCase2.java
+++ src/com/packagesgitDemo/TestCase2.java
@@ -3,7 +3,8 @@ package com.packagesgitDemo;
 public class TestCase2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Tester");
 		
 		System.out.println("Hi Prem My Code is Done Please review the same");
 
